f240181ab269508e217ce1349817f42bb061d7cc
Thais Alessandra <taaysalessandra@hotmail.com>
2022-05-13 02:55:28 -0300

    13/05-arrays

---
+++ src/exercicios/ARRAYS.java
@@ -0,0 +1,13 @@
+package exercicios;
+
+public class ARRAYS {
+    public static void main(String[] args) {
+        int[] vet={3, 5, 6, 8,412};
+        int count=0;
+        System.out.print("Vetor:");
+        while(count < (vet.length)){
+            System.out.print(vet[count]+" ");
+            count++;
+        }
+    }
+}
