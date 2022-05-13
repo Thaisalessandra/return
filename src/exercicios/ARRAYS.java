ff27550488b05d70916a4fc0308009dcff86f80c
Thais Alessandra <taaysalessandra@hotmail.com>
2022-05-13 03:17:43 -0300

    13/05-arrays

--- src/exercicios/ARRAYS.java
+++ src/exercicios/ARRAYS.java
@@ -4,10 +4,17 @@ public class ARRAYS {
     public static void main(String[] args) {
         int[] vet={3, 5, 6, 8,412};
         int count=0;
-        System.out.print("Vetor:");
+        System.out.print("Vetor: ");
         while(count < (vet.length)){
             System.out.print(vet[count]+" ");
             count++;
+
+        }
+        System.out.println(" ");
+        System.out.print("Vetor na ordem decrescente: ");
+        for (int i=(vet.length-1);i>=0;i--){
+            System.out.print(vet[i]+" ");
+
         }
     }
 }
